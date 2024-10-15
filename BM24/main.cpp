d9510b9b8d85a1ff8336c3b4a6ec577f6c3d57c4
Xpccccc <1014647664@qq.com>
2024-10-15 22:11:53 +0800

    BM24 二叉树的中序遍历

---
+++ BM24/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param root TreeNode类 
+     * @return int整型vector
+     */
+    vector<int> path;
+    vector<int> inorderTraversal(TreeNode* root) {
+        // write code here
+        preOrder(root);
+        return path;
+    }
+
+    void preOrder(TreeNode* root){
+        if(root == nullptr)
+            return ;
+        preOrder(root->left);
+        path.push_back(root->val);
+        preOrder(root->right);
+    }
+
+};
